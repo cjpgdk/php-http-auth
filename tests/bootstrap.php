162c7f89b7e5929e85dd54b8df12f2e1c50d09ab
Christian M. Jensen <cmj@cjpg.dk>
2017-05-20 14:34:02 +0200

    fix for travis build

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,13 @@
 <?php
 
-include_once '../src/CJPGDK/PhpHttpAuth/Database/DB.php';
-include_once '../src/CJPGDK/PhpHttpAuth/Database/MySQL.php';
-include_once '../src/CJPGDK/PhpHttpAuth/Database/PDODB.php';
+$basePath = realpath(dirname(__FILE__));
 
-include_once '../src/CJPGDK/PhpHttpAuth/Hash/Apr1Md5.php';
-//include_once '../src/CJPGDK/PhpHttpAuth/Hash/Md5.php';
-include_once '../src/CJPGDK/PhpHttpAuth/Hash/Sha.php';
-include_once '../src/CJPGDK/PhpHttpAuth/Hash/Bcrypt.php';
-include_once '../src/CJPGDK/PhpHttpAuth/HttpAuth.php';
+include_once $basePath.'/../src/CJPGDK/PhpHttpAuth/Database/DB.php';
+include_once $basePath.'/../src/CJPGDK/PhpHttpAuth/Database/MySQL.php';
+include_once $basePath.'/../src/CJPGDK/PhpHttpAuth/Database/PDODB.php';
+
+include_once $basePath.'/../src/CJPGDK/PhpHttpAuth/Hash/Apr1Md5.php';
+//include_once $basePath.'/../src/CJPGDK/PhpHttpAuth/Hash/Md5.php';
+include_once $basePath.'/../src/CJPGDK/PhpHttpAuth/Hash/Sha.php';
+include_once $basePath.'/../src/CJPGDK/PhpHttpAuth/Hash/Bcrypt.php';
+include_once $basePath.'/../src/CJPGDK/PhpHttpAuth/HttpAuth.php';
